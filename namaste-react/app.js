42f69ea0f4866f5bf269924eaf73693a3b28abfe
Nitesh Bagle <niteshbagle43@gmail.com>
2024-02-20 23:08:30 +0530

    food ordering app

--- namaste-react/app.js
+++ namaste-react/app.js
@@ -6,30 +6,35 @@ import ReactDOM from "react-dom/client";
 // JSX code is transpiled before it reaches to JS engine done by parcel - babel transpiler
 // React component --> class based and functional
 
-const App = () => (
-    <div>
-        <p id="heading">Namaste React App!</p>
-    </div>
-);
+// Header - logo, nav items - home, about, cart
+// body - search component, food cards (card container)
+// footer - copyright, link, contacts, support details, address 
 
-// function App () {
-//     return (
-//         <div>
-//             <p id="heading">Namaste React App!</p>
-//         </div> 
-//     );
-// }
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src="https://image.freepik.com/free-vector/food-app-icon-logo_29069-50.jpg"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    )
+}
+ReactDOM.createRoot(document.getElementById("root")).render(<AppLayout />);
 
-// class App extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <p id="heading">Namaste React App!</p>
-//             </div>
-//         )
-//     }
-// }
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
 
