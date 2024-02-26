12aaba4eabc453aec92eed2a74c18e0c0aff1b0f
Nitesh Bagle <niteshbagle43@gmail.com>
2024-02-26 22:49:58 +0530

    app structure changes

---
+++ namaste-react/src/utils/constants.js
@@ -0,0 +1,15 @@
+export const RESPONSE_DATA = [{
+    name: "Meghna Foods",
+    cuisines: "North Indian, Asian",
+    rating: "4.4 Stars",
+    eta: "38 minutes"
+}, {
+    name: "KFC",
+    cuisines: "Chicken Special",
+    rating: "4.0 Stars",
+    eta: "45 minutes"
+}];
+
+export const CDN_URL = 'https://www.fusion6.com.au/wp-content/uploads/2022/09/popular-indian-dishes-cuisine.jpg';
+
+export const LOGO_URL = ' https://image.freepik.com/free-vector/food-app-icon-logo_29069-50.jpg';
