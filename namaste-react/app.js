1404829efe6fa58777b6750495e1e1688d9dffbe
Nitesh Bagle <niteshbagle43@gmail.com>
2024-04-16 22:08:24 +0530

    react app basics

---
+++ namaste-react/app.js
@@ -0,0 +1,42 @@
+const heading = React.createElement(
+    "div", 
+    { id: "parent" }, 
+    [
+        React.createElement(
+            "div",
+            { id: "child", key: "div1" },
+            [
+                React.createElement(
+                    "h1",
+                    { id: "heading", key: "div1-h1" }, 
+                    "Hello World from div1-h1"
+                ),
+                React.createElement(
+                    "h2",
+                    { id: "heading", key: "div1-h2" }, 
+                    "Hello World from React again div1-h2"
+                )
+            ]
+        ),
+        React.createElement(
+            "div",
+            { id: "child", key: "div2" },
+            [
+                React.createElement(
+                    "h1",
+                    { id: "heading", key: "div2-h1" }, 
+                    "Hello World from React div2-h1"
+                ),
+                React.createElement(
+                    "h2",
+                    { id: "heading", key: "div2-h2" }, 
+                    "Hello World from React again div2-h2"
+                )
+            ]
+        )
+]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+// React Element (Javascript object) ==> HTML which browser understand
